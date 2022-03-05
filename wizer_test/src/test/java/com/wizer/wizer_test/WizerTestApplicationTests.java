7565af497cb0556b46b6b6822409c3bcec90a7dd
Kenoly <kelechi.a@spinel.consulting>
2022-03-05 11:49:16 +0100

    Initial commit

---
+++ wizer_test/src/test/java/com/wizer/wizer_test/WizerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizer.wizer_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
